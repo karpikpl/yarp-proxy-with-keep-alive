255166d8f65b2d6363e6f9d6d95076c6a02b8e0d
Piotr Karpala <piotrkarpala@microsoft.com>
2025-07-14 17:28:23 -0400

    yarp not working for ACA

--- yarp-proxy/Program.cs
+++ yarp-proxy/Program.cs
@@ -5,16 +5,40 @@ using Yarp.ReverseProxy;
 using System.Net.Http;
 using System.Net;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Use YARP with configuration from appsettings.json
+
+// Configure TCP keep-alive for all outbound YARP connections
 builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
+    .ConfigureHttpClient((context, handler) =>
+    {
+        if (handler is SocketsHttpHandler socketsHandler)
+        {
+            Console.WriteLine("Configuring SocketsHttpHandler for TCP keep-alive settings.");
+            socketsHandler.PooledConnectionIdleTimeout = TimeSpan.FromHours(2);
+            socketsHandler.ConnectTimeout = TimeSpan.FromHours(2);
+            socketsHandler.KeepAlivePingDelay = TimeSpan.FromSeconds(30); // TCP keep-alive
+            socketsHandler.KeepAlivePingTimeout = TimeSpan.FromSeconds(10);
+            socketsHandler.KeepAlivePingPolicy = HttpKeepAlivePingPolicy.Always;
+        }
+    });
+
+// Add a 2-hour timeout policy for proxied requests
+builder.Services.AddRequestTimeouts(options =>
+{
+    options.AddPolicy("2h", TimeSpan.FromHours(2));
+});
+
 
 
 var app = builder.Build();
 
+app.UseRequestTimeouts();
 
-app.MapReverseProxy();
+
+app.MapReverseProxy()
+.WithRequestTimeout(TimeSpan.FromHours(2));
 
 app.Run();
